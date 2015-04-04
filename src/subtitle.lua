c89cc0ad3c8adcd5b4c8f79d3f8462d5e31d6736
Douglas Rocha <douglasbellonrocha@gmail.com>
2015-04-04 12:45:05 -0300

    Creation of method that checks pattern of subtitle lines

--- src/subtitle.lua
+++ src/subtitle.lua
@@ -1,5 +1,11 @@
+require 'regex'
+require 'file'
+
 -- Creates regex pattern to determine which are the
 -- range sentence of srt segments
-REGEX_PATTERN = '\d+:\d+:\d+,\d+ --> \d+:\d+:\d+,\d+'
+--REGEX_PATTERN = '%d+:%d+:%d+,%d+ --> %d+:%d+:%d+,%d+'
+REGEX_PATTERN = '%d+:%d+:%d+,%d+ [-][-][>] %d+:%d+:%d+,%d+'
 
-
+function is_line_time_range(line)
+  return is_match(line, REGEX_PATTERN)
+end
